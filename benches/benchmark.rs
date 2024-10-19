52bddc3cb6f1530a4f79a03e570f798c04d45ed5
Ian <exor2008@yandex.ru>
2024-10-18 23:19:29 -0400

    Benchmark: intersection

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,5 +1,6 @@
 use std::array::from_fn;
 
+use bevy::math::{bounding::RayCast3d, Dir3A, Vec3A};
 use criterion::{criterion_group, criterion_main, Criterion};
 use oktree::{
     bounding::{Aabb, UVec3, Unsigned},
@@ -52,6 +53,24 @@ fn random_points() -> [DummyCell<usize>; RANGE] {
     })
 }
 
+fn random_rays() -> [RayCast3d; RANGE] {
+    let mut rnd = rand::thread_rng();
+    from_fn(|_| {
+        let x = rnd.gen_range(0.0..=RANGE as f32);
+        let y = rnd.gen_range(0.0..=RANGE as f32);
+        let z = rnd.gen_range(0.0..=RANGE as f32);
+        let origin = Vec3A::new(x, y, z);
+
+        let x_dir = rnd.gen_range(0.0..=1.0);
+        let y_dir = rnd.gen_range(0.0..=1.0);
+        let z_dir = rnd.gen_range(0.0..=1.0);
+        let direction = Vec3A::new(x_dir, y_dir, z_dir);
+        let direction = Dir3A::new(direction).unwrap();
+
+        RayCast3d::new(origin, direction, RANGE as f32)
+    })
+}
+
 fn octree_insert(points: [DummyCell<usize>; RANGE]) {
     let mut tree = Octree::from_aabb(Aabb::new(UVec3::splat(RANGE / 2), RANGE / 2));
 
@@ -72,12 +91,29 @@ fn octree_remove(points: [DummyCell<usize>; RANGE]) {
     }
 }
 
+fn octree_intersection(points: [DummyCell<usize>; RANGE]) {
+    let mut tree = Octree::from_aabb(Aabb::new(UVec3::splat(RANGE / 2), RANGE / 2));
+    let rays = random_rays();
+
+    for p in points {
+        let _ = tree.insert(p);
+    }
+
+    for ray in rays {
+        let _ = tree.ray_cast(&ray);
+    }
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     let points = random_points();
 
     c.bench_function("octree insert", |b| b.iter(|| octree_insert(points)));
 
     c.bench_function("octree remove", |b| b.iter(|| octree_remove(points)));
+
+    c.bench_function("octree intersection", |b| {
+        b.iter(|| octree_intersection(points))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
